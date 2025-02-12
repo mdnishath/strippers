94bc2267081fb0ca3f18749062f2e19f8e6475ee
mdnishath <wpprodeveloper@gmail.com>
2025-02-12 14:41:18 +0600

    images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["strippers.local"],
+    domains: ["strippers.local", "strippers.vercel.app"],
   },
 };
 
